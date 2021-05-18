c0545326f84eede138d76fde098fb75c56ca8ced
takefy-dev <jules001dixon@gmail.com>
2021-05-18 19:03:26 +0200

    change features into assets folder

---
+++ front/src/assets/features.js
@@ -0,0 +1,5 @@
+module.exports = [
+    {name: "Un puissant antiraid", img: "https://cdn.discordapp.com/attachments/820031925389230110/844176365845479434/dekuPunch.gif", description:"description"},
+    {name : "Une automodération" , img: "https://thumbs.gfycat.com/AlarmingTatteredKiwi-max-1mb.gif", description:"description"},
+    {name : "Des outils pour faciliter la gestion de votre serveur", img:"https://i.pinimg.com/originals/b8/24/77/b824773317a65cc50c85d942f88c8fe3.gif", description: "description"}
+]
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -19,18 +19,14 @@
 </template>
 
 <script>
-
+import features from '../assets/features'
 export default {
   name: 'Home',
   title: 'Acceuil',
 
   data () {
     return {
-      features : [
-        {name: "Un puissant antiraid", img: "https://cdn.discordapp.com/attachments/820031925389230110/844176365845479434/dekuPunch.gif", description:"description"},
-        {name : "Une automodération" , img: "https://thumbs.gfycat.com/AlarmingTatteredKiwi-max-1mb.gif", description:"description"},
-        {name : "Des outils pour faciliter la gestion de votre serveur", img:"https://i.pinimg.com/originals/b8/24/77/b824773317a65cc50c85d942f88c8fe3.gif", description: "description"}
-      ]
+      features
     }
   }
 }
