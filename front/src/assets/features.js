cdbfcdf09428c836b861c57182cb8c45b628a60e
rh#0002 <70811356+rheusss@users.noreply.github.com>
2021-05-18 19:22:30 +0200

    tttt

--- front/src/assets/features.js
+++ front/src/assets/features.js
@@ -1,5 +1,7 @@
 module.exports = [
-    {name: "Un puissant antiraid", img: "https://cdn.discordapp.com/attachments/820031925389230110/844176365845479434/dekuPunch.gif", description:"description"},
-    {name : "Une automodération" , img: "https://thumbs.gfycat.com/AlarmingTatteredKiwi-max-1mb.gif", description:"description"},
-    {name : "Des outils pour faciliter la gestion de votre serveur", img:"https://i.pinimg.com/originals/b8/24/77/b824773317a65cc50c85d942f88c8fe3.gif", description: "description"}
+    {name: "Un des + puissant antiraid", img: "https://cdn.discordapp.com/attachments/820031925389230110/844176365845479434/dekuPunch.gif", description:"description"},
+    {name : "Une automodération qui vous aide au quotidien" , img: "https://thumbs.gfycat.com/AlarmingTatteredKiwi-max-1mb.gif", description:"description"},
+    {name : "Des outils pour faciliter la gestion de votre serveur", img:"https://i.pinimg.com/originals/b8/24/77/b824773317a65cc50c85d942f88c8fe3.gif", description: "description"},
+    {name : "Un bot qui rassemble l'ensemble des autres", img:"https://giffiles.alphacoders.com/103/103147.gif", description: "description"},
+    {name : "Une expérience indédite, avec un des plus beau design", img:"https://giffiles.alphacoders.com/103/103142.gif", description: "description"},
 ]
